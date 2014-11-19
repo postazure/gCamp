61b551bf5a4385dcdb1a7042afc34b0734258b39
postazure <gordonmaxc@gmail.com>
2014-11-19 15:33:14 -0800

    Stage

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,12 @@
 class WelcomeController < ApplicationController
+
   def home
+    quotes = [
+      '"Immortality consists largely of boredom." -Zefrem Cochrane, "Metamorphosis", stardate 3219.8',
+      '"The best diplomat I know is a fully activated phaser bank." -Scotty',
+      '"I\'m a soldier, not a diplomat. I can only tell the truth." -Kirk, "Errand of Mercy", stardate 3198.9',
+    ]
+
+    render(:home, locals:{quotes: quotes})
   end
 end

343656f576845c222bfeaca006587fc89d988696
postazure <gordonmaxc@gmail.com>
2014-11-20 17:02:39 -0800

    Fixed Stufff

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,12 +1,9 @@
 class WelcomeController < ApplicationController
   def home
     quotes = [
-      ['"Immortality consists largely of boredom."',
-        'Zefrem Cochrane, "Metamorphosis", stardate 3219.8'],
-      ['"The best diplomat I know is a fully activated phaser bank."',
-        ' Scotty'],
-      ['"I\'m a soldier, not a diplomat. I can only tell the truth."',
-        ' Kirk, "Errand of Mercy", stardate 3198.9'],
+      ['Failure is not an option. Everyone has to Succeed', 'Arnold Schwarzenegger'],
+      ['Your time is limited, so don\'t waste it living someone else\'s life.', 'Steve Jobs'],
+      ['Better Ingredients, Better Pizza', 'Papa John\'s'],
     ]
 
     render(:home, locals:{quotes: quotes})

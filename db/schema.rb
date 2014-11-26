ac9f37bec9e7e3051215bfe5956d833c1feecd28
postazure <gordonmaxc@gmail.com>
2014-11-26 14:23:35 -0800

    Add due date & complete to show and index

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141126212049) do
+ActiveRecord::Schema.define(version: 20141126213754) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20141126212049) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.date     "due_date"
+    t.boolean  "completed"
   end
 
 end

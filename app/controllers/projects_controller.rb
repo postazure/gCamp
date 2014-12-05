5300a866bec1369a0acca09fef17a6ac7e847441
postazure <gordonmaxc@gmail.com>
2014-12-05 15:21:43 -0800

    project create and index

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -5,4 +5,22 @@ class ProjectsController < ApplicationController
   def new
     @project = Project.new
   end
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      redirect_to @project, notice: 'Project was successfully created.'
+    else
+      render :new
+    end
+  end
+  def show
+    @project = Project.find(params[:id])
+  end
+
+
+
+  private
+    def project_params
+      params.require(:project).permit(:name)
+    end
 end

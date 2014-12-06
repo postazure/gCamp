9ff57e2ed1aed6bc0a55b55ca71c6826c5e60bef
postazure <gordonmaxc@gmail.com>
2014-12-05 16:16:26 -0800

    edit projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -16,6 +16,19 @@ class ProjectsController < ApplicationController
     end
   end
 
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:id])
+    if @project.update(project_params)
+      redirect_to @project, notice: 'Project was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
   def show
     @project = Project.find(params[:id])
   end

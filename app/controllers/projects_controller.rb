dada7a4a8c7b9f36d7276525922cf4108440e7c3
postazure <gordonmaxc@gmail.com>
2014-12-05 16:05:33 -0800

    can delete users

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,9 +2,11 @@ class ProjectsController < ApplicationController
   def index
     @projects = Project.all
   end
+
   def new
     @project = Project.new
   end
+
   def create
     @project = Project.new(project_params)
     if @project.save
@@ -13,8 +15,15 @@ class ProjectsController < ApplicationController
       render :new
     end
   end
+
   def show
     @project = Project.find(params[:id])
+  end
+
+  def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+    redirect_to projects_url, notice: 'Project was successfully deleted.'
   end
 
 

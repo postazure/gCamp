4e5a4979268dddc4dc0fcad36ac7c60c7c52beef
Max Gordon <gordonmaxc@gmail.com>
2014-12-09 14:10:33 -0800

    validate future date for new task

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,11 @@
 class Task < ActiveRecord::Base
   validates :description, presence: true
+  validate :due_date_cannot_be_in_the_past, on: :create
+
+
+  def due_date_cannot_be_in_the_past
+    if due_date.present? && due_date < Date.today
+      errors.add(:due_date, "must be a future date.")
+    end
+  end
 end

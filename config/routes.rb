d5cf7c75dbce3befbe8dfe280ed22b1ad14e2e7b
Max Gordon <gordonmaxc@gmail.com>
2014-12-17 14:00:15 -0800

    signin

--- config/routes.rb
+++ config/routes.rb
@@ -13,8 +13,8 @@ Rails.application.routes.draw do
 
   get   "/signup" => "registrations#new"
   post  "/signup" => "registrations#create"
-  # get   "/signin" => "authentication#new"
-  # post  "/signin" => "authentication#create"
+  get   "/signin" => "authentication#new"
+  post  "/signin" => "authentication#create"
   get   "/signout" => "authentication#destroy"
 
 

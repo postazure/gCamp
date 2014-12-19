352bf4cca937ce5788f83379d495aa0f92d21707
Max Gordon <gordonmaxc@gmail.com>
2014-12-18 18:09:16 -0800

    test signup

--- spec/features/auth_spec.rb
+++ spec/features/auth_spec.rb
@@ -1,25 +1,34 @@
 require "rails_helper"
 
 feature "authorization" do
-  it "nav to registration page" do
-    visit root_path
-    click_on "user-registration-action"
+  feature "registrations#new" do
+    it "register with valid info" do
+      visit root_path
+      click_on "user-registration-action"
+      fill_in :user_first_name, with: "John"
+      fill_in :user_last_name, with: "Smith"
+      fill_in :user_email, with: "g@g.com"
+      fill_in :user_password, with: "abc123"
+      fill_in :user_password_confirmation, with: "abc123"
 
-    expect(page.current_path).to eq(signup_path)
-  end
 
-  it "register a user" do
-    visit root_path
-    click_on "user-registration-action"
-    fill_in :user_first_name, with: "John"
-    fill_in :user_last_name, with: "Smith"
-    fill_in :user_email, with: "g@g.com"
-    fill_in :user_password, with: "abc123"
-    fill_in :user_password_confirmation, with: "abc123"
-    
+      click_on "submit-user"
 
-    click_on "submit-user"
+      expect(page).to have_content("Sign Out")
+    end
 
-    expect(page).to have_content("Sign Out")
+    it "register with invalid info" do
+      visit root_path
+      click_on "user-registration-action"
+      # fill_in :user_first_name, with: "John"
+      # fill_in :user_last_name, with: "Smith"
+      fill_in :user_email, with: "g@g.com"
+      fill_in :user_password, with: "abc123"
+      # fill_in :user_password_confirmation, with: "abc123"
+      click_on "submit-user"
+
+      expect(page).to have_content("errors prohibited")
+      expect(page.current_path).to eq(signup_path)
+    end
   end
 end

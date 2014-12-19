366047b119ea1e9d9ad01485dcbca9215bc3eb2e
Max Gordon <gordonmaxc@gmail.com>
2014-12-18 22:08:03 -0800

    signin test suite

--- spec/features/auth_spec.rb
+++ spec/features/auth_spec.rb
@@ -32,38 +32,39 @@ feature "authorization" do
   end
 
   feature AuthenticationController do
-    it "sign in with valid login" do
-      test_user = User.create!(
-        first_name: "Buster",
-        last_name: "Bluth",
-        email:"buster@bluthcompany.com",
-        password:"the blue is land",
-        password_confirmation:"the blue is land",
-      )
+    feature "signin" do
+      scenario "sign in with valid login" do
+        test_user = User.create!(
+          first_name: "Buster",
+          last_name: "Bluth",
+          email:"buster@bluthcompany.com",
+          password:"the blue is land",
+          password_confirmation:"the blue is land",
+        )
 
-      visit root_path
-      click_on "user-signin-action"
-      fill_in :email, with: "buster@bluthcompany.com"
-      fill_in :password, with: "the blue is land"
-      click_on "submit-user"
+        visit root_path
+        click_on "user-signin-action"
+        fill_in :email, with: "buster@bluthcompany.com"
+        fill_in :password, with: "the blue is land"
+        click_on "submit-user"
 
-      expect(page).to have_content("Sign Out")
-      expect(page).to have_no_content("Sign In")
-      expect(page.current_path).to eq(root_path)
+        expect(page).to have_content("Sign Out")
+        expect(page).to have_no_content("Sign In")
+        expect(page.current_path).to eq(root_path)
+      end
+
+      scenario "sign in with invalid login" do
+        visit root_path
+        click_on "user-signin-action"
+        fill_in :email, with: "gob@bluthcompany.com"
+        fill_in :password, with: "Ive made a horrible mistake"
+        click_on "submit-user"
+
+        expect(page).to have_content("Username / password combination is invalid")
+        expect(page).to have_content("Sign In")
+        expect(page).to have_no_content("Sign Out")
+        expect(page.current_path).to eq(signin_path)
+      end
     end
-
-    it "sign in with invalid login" do
-      visit root_path
-      click_on "user-signin-action"
-      fill_in :email, with: "gob@bluthcompany.com"
-      fill_in :password, with: "Ive made a horrible mistake"
-      click_on "submit-user"
-
-      expect(page).to have_content("Username / password combination is invalid")
-      expect(page).to have_content("Sign In")
-      expect(page).to have_no_content("Sign Out")
-      expect(page.current_path).to eq(signin_path)
-    end
-
   end
 end

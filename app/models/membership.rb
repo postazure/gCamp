b0db2866021a26abe2326d2754d7692af9d77dc5
Max Gordon <gordonmaxc@gmail.com>
2014-12-19 16:27:45 -0800

    add validation no duplicated users on projects

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -4,4 +4,11 @@ class Membership < ActiveRecord::Base
   # belongs_to :role
 
   validates :user_id, presence: true
+  validate :user_id_uniqueness_to_project
+
+  def user_id_uniqueness_to_project
+    if :user_id.present? && Membership.where(project_id: project_id, user_id: user_id).count > 0
+      errors.add(:user_id, "already a member of this project")
+    end
+  end
 end
